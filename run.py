7edb2e8904b702f064eaaf042b25666e4c3d1f95
Anzela Siniavskaja <v6zmaqkl@students.codeinstitute.net>
2024-10-04 11:38:02 +0000

    create a game board

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+#Tic-Tac-Toe
+
+board_size = 3
+
+board = [1,2,3,4,5,6,7,8,9]
+
+
+
+def draw_board():
+	print (('_' * 4 * board_size ))
+	for i in range(board_size):
+		print ((' ' * 3 + '|') * 3)
+		print ('',board[i*3], '|', board[1+i*3], '|', board[2+i*3], '|')
+		print (('_' * 3 + '|') * 3)
+
+
+def start_game():
+    draw_board()
+
+print("Welcome to the game")
+start_game()
